9dad345ff976cedbd234e771bfb5e9547e2e5115
Mai <anna-maija.rauramaa@aalto.fi>
2020-01-07 16:19:52 +0200

    Sandboxing with React Boostrap, first images there

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Container, Image, Row, Col } from 'react-bootstrap'
+
 import watercolor from './pics/watercol_texture.jpg'
 import dock from './pics/bg_elems_cabin.png'
 import iland from './pics/bg_iland.png'
 import mountains from './pics/bg_mountains.png'
 import divers from './pics/divers_graphic1.png'
+import seashells from './pics/seashells_cabarete.jpg'
 
 const bgStyle = {
   //backgroundImage: `url(${watercolor})`,
@@ -17,13 +20,19 @@ const bgStyle = {
 
 const App = () => {
   return (
-    <div>
-      <img style={bgStyle} src={watercolor}/>
-      <img src={dock}/>
-      <img src={iland}/>
-      <img src={mountains}/>
-      <img src={divers}></img>
-    </div>
+    <Container>
+      <ImageBox img={seashells}/>
+      <ImageBox img={dock}/>
+      <p>Lorem aya hipsum yolo spatula cupcake</p>
+    </Container>
+  )
+}
+
+
+// eslint-disable-next-line react/prop-types
+const ImageBox = ({img}) => {
+  return (
+    <Image src={img} fluid/>
   )
 }
 
