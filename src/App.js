643977d399b6c52fb416da90521fe6293c85e578
Mai <anna-maija.rauramaa@aalto.fi>
2020-01-07 16:43:28 +0200

    Page layout grid 1st steps

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import dock from './pics/bg_elems_cabin.png'
 import iland from './pics/bg_iland.png'
 import mountains from './pics/bg_mountains.png'
 import divers from './pics/divers_graphic1.png'
+import blue_shell from './pics/seashell_blue.jpg'
 import seashells from './pics/seashells_cabarete.jpg'
 
 const bgStyle = {
@@ -17,12 +18,40 @@ const bgStyle = {
   backgroundRepeat: 'no-repeat'
 }
 
+const containerStyle = {
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'yellow',
+  display: 'flex',
+  //alignItems: 'center',
+  justifyContent: 'center'
+}
+
+const rowStyle = {
+  width: '50%',
+  height: '100%',
+  borderRadius: '1em',
+  backgroundColor: 'blue',
+  margin: '0em',
+  padding: '1em'
+}
+
+const gridElemStyle = {
+  width: '90%',
+  height: '100%',
+  borderRadius: '1em',
+  backgroundColor: 'red',
+  padding: '0.5em',
+  margin: '0.25em'
+}
+
 
 const App = () => {
   return (
     <Container>
-      <ImageBox img={seashells}/>
+      <Grid/>
       <ImageBox img={dock}/>
+
       <p>Lorem aya hipsum yolo spatula cupcake</p>
     </Container>
   )
@@ -32,7 +61,23 @@ const App = () => {
 // eslint-disable-next-line react/prop-types
 const ImageBox = ({img}) => {
   return (
-    <Image src={img} fluid/>
+    <Image src={img}/>
+  )
+}
+
+const Grid = () => {
+  return(
+    <Container style={containerStyle}>
+      <Row style={rowStyle} >
+        <Col sm style={gridElemStyle}><ImageBox img={blue_shell}/></Col>
+        <Col sm style={gridElemStyle}>Seashell Macaroni Desperate Pancake</Col>
+      </Row>
+      <Row style={rowStyle} >
+        <Col style={gridElemStyle}>Blue Whale Yellow Hellow Ocean Strategy</Col>
+        <Col style={gridElemStyle}>Motivation Whale Nightingale</Col>
+        <Col style={gridElemStyle}>Pizza Romana Masco Tri Colorana</Col>
+      </Row>
+    </Container>
   )
 }
 
