9cbf626c116ae2f4d3a6e282b8744d0a0b1dabc6
Mai <anna-maija.rauramaa@aalto.fi>
2020-01-07 17:43:04 +0200

    Website layout Grid MVP

--- src/App.js
+++ src/App.js
@@ -10,72 +10,112 @@ import blue_shell from './pics/seashell_blue.jpg'
 import seashells from './pics/seashells_cabarete.jpg'
 
 const bgStyle = {
-  //backgroundImage: `url(${watercolor})`,
+  margin: '0',
   width: '100%',
-  height: '100%',
+  height: '35em',
+  backgroundImage: `url(${watercolor})`,
+  //display: 'flex',
+  //alignItems: 'center',
+  justifyContent: 'center',
   backgroundPosition: 'center',
   backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat'
+  backgroundRepeat: 'no-repeat',
+  //overflow: 'hidden',
+  //opacity: '0.8',
 }
 
 const containerStyle = {
+  margin: '0',
+  padding: '0',
   width: '100%',
-  height: '100%',
-  backgroundColor: 'yellow',
+  minHeight: '100%',
+  //backgroundImage: `url(${watercolor})`,
   display: 'flex',
   //alignItems: 'center',
-  justifyContent: 'center'
+  justifyContent: 'center',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  overflow: 'hidden' //change this to get scrollable
 }
 
 const rowStyle = {
-  width: '50%',
+  width: '33%',
   height: '100%',
   borderRadius: '1em',
+  //backgroundColor: 'blue',
+  margin: '0em',
+  padding: '1em'
+}
+
+const fullWidthRow = {
+  width: '95%',
+  height: '1em',
+  borderRadius: '0em',
   backgroundColor: 'blue',
   margin: '0em',
   padding: '1em'
 }
 
+const imgStyle = {
+  width: '120%',
+  height: '200%',
+  padding: '0',
+  margin: '0'
+}
+
 const gridElemStyle = {
-  width: '90%',
-  height: '100%',
-  borderRadius: '1em',
-  backgroundColor: 'red',
-  padding: '0.5em',
-  margin: '0.25em'
+  width: '95%',
+  height: '8em',
+  //borderRadius: '1em',
+  //backgroundColor: 'red',
+  //padding: '0.5em',
+  margin: '0.05em'
 }
 
 
 const App = () => {
   return (
-    <Container>
-      <Grid/>
-      <ImageBox img={dock}/>
-
-      <p>Lorem aya hipsum yolo spatula cupcake</p>
+    <Container style={bgStyle}>
+      <Grid />
     </Container>
   )
 }
 
-
 // eslint-disable-next-line react/prop-types
-const ImageBox = ({img}) => {
+const ImageBox = ({ image, style }) => {
   return (
-    <Image src={img}/>
+    <Image src={image} style={style} />
   )
 }
 
 const Grid = () => {
-  return(
+  return (
     <Container style={containerStyle}>
       <Row style={rowStyle} >
-        <Col sm style={gridElemStyle}><ImageBox img={blue_shell}/></Col>
-        <Col sm style={gridElemStyle}>Seashell Macaroni Desperate Pancake</Col>
+        <Col style={gridElemStyle}></Col>
+        <Col style={gridElemStyle}></Col>  
+        <Col style={gridElemStyle}>
+          <ImageBox image={iland} style={{opacity: '0.8', height: '100%'}}/>
+        </Col>   
+        
+        
+      </Row>
+      <Row style={rowStyle}>
+        <Col style={gridElemStyle}>
+        </Col>
+        <Col style={gridElemStyle}>
+          <ImageBox image={mountains} style={{opacity: '0.5'}}/>
+        </Col>
+        <Col style={gridElemStyle}>
+          <ImageBox image={divers} style={imgStyle}/>
+        </Col>
       </Row>
       <Row style={rowStyle} >
-        <Col style={gridElemStyle}>Blue Whale Yellow Hellow Ocean Strategy</Col>
-        <Col style={gridElemStyle}>Motivation Whale Nightingale</Col>
-        <Col style={gridElemStyle}>Pizza Romana Masco Tri Colorana</Col>
+        <Col style={gridElemStyle}></Col>
+        <Col style={gridElemStyle}>
+          <ImageBox image={dock} />
+        </Col>
       </Row>
     </Container>
   )
