201af00c7f17bf66efc6e5b9ac2442e02d46c8be
Mai <anna-maija.rauramaa@aalto.fi>
2020-01-07 16:01:48 +0200

    Sandboxing with images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import watercolor from './pics/watercol_texture.jpg'
+import dock from './pics/bg_elems_cabin.png'
+import iland from './pics/bg_iland.png'
+import mountains from './pics/bg_mountains.png'
+import divers from './pics/divers_graphic1.png'
+
+const bgStyle = {
+  //backgroundImage: `url(${watercolor})`,
+  width: '100%',
+  height: '100%',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat'
+}
+
 
 const App = () => {
   return (
-    <div >
-      Yolo hipsum solo quark
+    <div>
+      <img style={bgStyle} src={watercolor}/>
+      <img src={dock}/>
+      <img src={iland}/>
+      <img src={mountains}/>
+      <img src={divers}></img>
     </div>
   )
 }
